de92853f74ce33bc3aee3e673d0d8779e628f9b1
jamesshieh <randomletters13@gmail.com>
2013-01-22 13:59:31 -0800

    added intro gamestate

---
+++ lib/hm/gamestates/intro.rb
@@ -0,0 +1,9 @@
+class Intro < Chingu::GameState
+  def initialize
+    super
+    @title = Chingu::Text.create(:text => "Harvest Monsters v0.0", :x => 100, :y => 100, :size => 100)
+    self.input =  { :escape => :exit,
+                  }
+  end
+end
+
